e3ad166ea597b619352f51a17f03accd3e53da1c
Zumry Mohamed <zumrywahid@gmail.com>
2021-04-21 12:41:00 +0800

    Hard coded the loitering value.

--- android/src/main/kotlin/io/flutter/plugins/geofencing/GeofencingPlugin.kt
+++ android/src/main/kotlin/io/flutter/plugins/geofencing/GeofencingPlugin.kt
@@ -92,7 +92,7 @@ class GeofencingPlugin : ActivityAware, FlutterPlugin, MethodCallHandler {
               .setRequestId(id)
               .setCircularRegion(lat, long, radius)
               .setTransitionTypes(fenceTriggers)
-              .setLoiteringDelay(loiteringDelay)
+              .setLoiteringDelay(30 * 1000)
               .setNotificationResponsiveness(notificationResponsiveness)
               .setExpirationDuration(expirationDuration)
               .build()
