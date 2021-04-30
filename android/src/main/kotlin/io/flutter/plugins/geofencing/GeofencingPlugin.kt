8e441f365b3fd6b71c07933dab662cdfb9e10f98
Zumry Mohamed <zumrywahid@gmail.com>
2021-04-30 10:35:55 +0800

    Update GeofencingPlugin.kt

--- android/src/main/kotlin/io/flutter/plugins/geofencing/GeofencingPlugin.kt
+++ android/src/main/kotlin/io/flutter/plugins/geofencing/GeofencingPlugin.kt
@@ -220,7 +220,7 @@ class GeofencingPlugin : ActivityAware, FlutterPlugin, MethodCallHandler {
     private fun setDeviceAlarmLevel(context: Context, result: Result) {
       try {
         val audioManager: AudioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        audioManager.setStreamVolume(AudioManager.STREAM_ALARM, audioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM), 0);
+        audioManager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, 1, 0);
       } catch (e: Exception) {
         e.printStackTrace();
       }
